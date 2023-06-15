2cc88f4e717ba07857c481e8c2b544e81c636c79
nayanbisht <91422708+nayanbisht@users.noreply.github.com>
2023-06-15 16:05:34 +0530

    Add files via upload

---
+++ lib/elements/colors.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MainColor{
+  static Color primaryColor = Color(0xFFff4b4b);
+  static Color secondaryColor = Color(0xFFffca27);
+  static Color accentColor = Color(0xFF4169e8);
+}
